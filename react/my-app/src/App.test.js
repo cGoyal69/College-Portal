1115efaf57b808c7b3192f677bbd8505759cedb5
cGoyal69 <cgoy96@gmail.com>
2024-01-28 00:01:08 +0530

    Added Login System

--- react/my-app/src/App.test.js
+++ react/my-app/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Header from './App';
+import MyApp from './App';
+import {TodoList} from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<TodoList />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
