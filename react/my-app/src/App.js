2a43d5f0cdb0883a4efb7f3db1b8915610e95aae
cGoyal69 <cgoy96@gmail.com>
2024-01-28 13:12:25 +0530

    aligning header

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -2,35 +2,16 @@ export default function MyApp() {
   return(
     <>
     <header>
+      <div className="logo">
     <img src="iiitm.png"/>
+    </div>
     <>
     <h1><div className="hindi">भारतीय सूचना प्रौद्योगिकी संस्थान सेनापति, मणिपुर<br/></div>
-    Indian Institute for Information Technology, Senapati, Manipur
-    <br/>
+    <div className="english">Indian Institute for Information Technology, Senapati, Manipur<br/></div>
     <div className="chhotaheading">(An Institute of National Importance by Act of Parliament, Govt. of India)</div>
     </h1>
-    
     </>
-
     </header>
-    <TodoList />
     </>
-  );
-}
-export function TodoList() 
-{
-  return(
-  <div>
-   <img src="https://qph.cf2.quoracdn.net/main-qimg-9959f634b80f96751e14720856806434-lq" usemap="#image-map"/>
-            <map name="image-map">
-                <area target="_blank" alt="Auditorium" title="Auditorium" href="http://www.iiitmanipur.ac.in/" coords="205,137,494,249" shape="rect"/>
-                <area target="_blank" alt="Academic Block" title="Academic Block" href="http://www.iiitmanipur.ac.in/" coords="0,150,207,200" shape="rect"/>
-                <area target="_blank" alt="PLayground" title="Playground" href="http://www.iiitmanipur.ac.in/" coords="0,200,202,250" shape="rect"/>
-                <area target="_blank" alt="Imphal" title="Imphal" href="https://www.google.com/search?client=safari&rls=en&q=Imphal&ie=UTF-8&oe=UTF-8" coords="0,0,243,150" shape="rect"/>
-                <area target="_blank" alt="Imphal" title="Imphal" href="https://www.google.com/search?client=safari&rls=en&q=Imphal&ie=UTF-8&oe=UTF-8" coords="243,0,520,138" shape="rect"/>
-                <area target="_blank" alt="Imphal" title="Imphal" href="https://www.google.com/search?client=safari&rls=en&q=Imphal&ie=UTF-8&oe=UTF-8" coords="525,117,602,223" shape="rect"/>
-                <area target="_blank" alt="Rainbow" title="Rainbow" href="https://www.google.com/search?client=safari&rls=en&q=rainbow&ie=UTF-8&oe=UTF-8" coords="492,0,602,138" shape="rect"/>
-            </map>
-  </div>
   );
 }
