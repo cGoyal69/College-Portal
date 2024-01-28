d9fae3a03a0f81d74c773add8cd9df339f268400
cGoyal69 <cgoy96@gmail.com>
2024-01-28 22:02:55 +0530

    designing header logo

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,13 +1,15 @@
-export default function MyApp() {
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+export default function Header() {
   return(
     <>
     <header>
-      <div className="logo">
-    <img src="iiitm.png"/>
+    <div className="logo">
+    <a href="http://www.iiitmanipur.ac.in/"><img className="imgLogo" src="iiitm.png" alt="logo"/></a>
     </div>
     <>
     <h1><div className="hindi">भारतीय सूचना प्रौद्योगिकी संस्थान सेनापति, मणिपुर<br/></div>
-    <div className="english">Indian Institute for Information Technology, Senapati, Manipur<br/></div>
+    <div className="english">Indian Institute for Information Technology Senapati, Manipur<br/></div>
     <div className="chhotaheading">(An Institute of National Importance by Act of Parliament, Govt. of India)</div>
     </h1>
     </>
--- react/my-app/src/App.test.js
+++ react/my-app/src/App.test.js
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import {App} from './App';
 import Header from './App';
 import MyApp from './App';
-import {TodoList} from './App';
+import { LoginBox } from './App';
 
 test('renders learn react link', () => {
-  render(<TodoList />);
+  render(<MyApp />);
+  render(<App />)
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
