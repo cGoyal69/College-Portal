6532d668c5b30724b780c4ba7bf168da2500fff8
cGoyal69 <cgoy96@gmail.com>
2024-01-31 10:45:36 +0530

    creating header function

---
+++ react/my-app/src/Header.js
@@ -0,0 +1,14 @@
+export function Header(){
+    return(
+    <header>
+      <div className="logo">
+        <a href="http://www.iiitmanipur.ac.in/"><img className="imgLogo" src="iiitm.png" alt="logo"/></a>
+      </div>
+      <h1>
+        <div className="hindi">भारतीय सूचना प्रौद्योगिकी संस्थान सेनापति, मणिपुर<br/></div>
+        <div className="english">Indian Institute for Information Technology Senapati, Manipur<br/></div>
+        <div className="chhotaheading">(An Institute of National Importance by Act of Parliament, Govt. of India)</div>
+      </h1>
+    </header>
+    );
+}
