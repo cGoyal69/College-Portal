19e6e7a989f7ff6f81c584001e55d7bd835dbe5c
cGoyal69 <cgoy96@gmail.com>
2024-01-31 15:15:30 +0530

    student page

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Student from './student'
 import{Login} from './Login'
-import{Header} from './Header'
  
 export default function App() {
   return(
