6bb02bcfe27ca1bddffbabad8695a6fa3f5db6f1
cGoyal69 <cgoy96@gmail.com>
2024-02-01 18:03:36 +0530

    profile

--- react/my-app/src/profile.js
+++ react/my-app/src/profile.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import { Header } from "./Header";
+import { Navbar } from "./NavBar";
 
-export default function Profile(){
-    return(
-        <>
-        <h1>Hello This is profile page</h1> 
-        </>
-    );
+export default class Profile extends Component {
+    render()
+    {
+        return(
+            <>
+            <Header theme="headerch"/>
+            <Navbar/>
+            </>
+        );
+    }
 }
