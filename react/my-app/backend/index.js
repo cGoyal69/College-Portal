3c8f010049eb5762babec6838ce794652c8767e1
HarshKd <harshkumardas24@gmail.com>
2024-03-14 17:51:46 +0530

    backend variable export

--- react/my-app/backend/index.js
+++ react/my-app/backend/index.js
@@ -1,66 +1,27 @@
-// Code for mongoose config in backend
-// Filename - backend/index.js
 
-// To connect with your mongoDB database
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/', {
-	dbName: 'yourDB-name',
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-}, err => err ? console.log(err) : 
-	console.log('Connected to yourDB-name database'));
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://college:college123@myatlasclusteredu.tchpdvk.mongodb.net/?retryWrites=true&w=majority";
 
-// Schema for users of app
-const UserSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	date: {
-		type: Date,
-		default: Date.now,
-	},
-});
-const User = mongoose.model('users', UserSchema);
-User.createIndexes();
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
 
-// For backend and express
-const express = require('express');
-const app = express();
-const cors = require("cors");
-console.log("App listen at port 5000");
-app.use(express.json());
-app.use(cors());
-app.get("/", (req, resp) => {
-
-	resp.send("App is Working");
-	// You can check backend is working or not by 
-	// entering http://loacalhost:5000
-	
-	// If you see App is working means
-	// backend working properly
-});
-
-app.post("/register", async (req, resp) => {
+async function run(user) { 
+	let a = "Error";
 	try {
-		const user = new User(req.body);
-		let result = await user.save();
-		result = result.toObject();
-		if (result) {
-			delete result.password;
-			resp.send(req.body);
-			console.log(result);
-		} else {
-			console.log("User already register");
-		}
-
-	} catch (e) {
-		resp.send("Something Went Wrong");
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		const database = client.db('studentdata');
+		const studentdata = database.collection('studentdata');
+		const query = { username: user}
+		a = await studentdata.findOne(query);
+		console.log(a)
+		// Send a ping to confirm a successful connection
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} finally {
+		await client.close();
 	}
-});
-app.listen(5000);
+	return a;
+}
+
+const studentDataset = run().catch(console.dir);
+export {studentDataset};
