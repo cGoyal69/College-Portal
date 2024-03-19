cdfbc73f8f4b3b3c8e102bd3397369c863a5d230
cGoyal69 <cgoy96@gmail.com>
2024-03-20 00:50:47 +0530

    backend

--- react/my-app/backend/index.js
+++ react/my-app/backend/index.js
@@ -1,66 +1,30 @@
-// Code for mongoose config in backend
-// Filename - backend/index.js
-
-// To connect with your mongoDB database
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/', {
-	dbName: 'yourDB-name',
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-}, err => err ? console.log(err) : 
-	console.log('Connected to yourDB-name database'));
-
-// Schema for users of app
-const UserSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	date: {
-		type: Date,
-		default: Date.now,
-	},
-});
-const User = mongoose.model('users', UserSchema);
-User.createIndexes();
-
-// For backend and express
-const express = require('express');
-const app = express();
-const cors = require("cors");
-console.log("App listen at port 5000");
+const express = require('express')
+const app = express()
 app.use(express.json());
-app.use(cors());
-app.get("/", (req, resp) => {
+const momgoose = require("mongoose");
 
-	resp.send("App is Working");
-	// You can check backend is working or not by 
-	// entering http://loacalhost:5000
-	
-	// If you see App is working means
-	// backend working properly
+
+
+app.post("/", async(req, res) => {
+  console.log(req.body);
+  const {data} = req.body;
+
+try{
+  if(data == "HKD"){
+   res.send({status:"ok"}) 
+  }
+  else{
+    res.send({status:"User Not Found"})
+  }
+}
+catch(error){
+  res.send({status:"error"})
+}
 });
 
-app.post("/register", async (req, resp) => {
-	try {
-		const user = new User(req.body);
-		let result = await user.save();
-		result = result.toObject();
-		if (result) {
-			delete result.password;
-			resp.send(req.body);
-			console.log(result);
-		} else {
-			console.log("User already register");
-		}
 
-	} catch (e) {
-		resp.send("Something Went Wrong");
-	}
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 });
-app.listen(5000);
