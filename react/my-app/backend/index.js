8490de6188b99373f17039493a10aa227b79955b
cGoyal69 <cgoy96@gmail.com>
2024-03-21 19:31:14 +0530

    backend data fetch

--- react/my-app/backend/index.js
+++ react/my-app/backend/index.js
@@ -1,42 +1,43 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
 app.use(express.json());
 
-const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://college:college123@myatlasclusteredu.tchpdvk.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri);
 
 async function run(user) { 
-	let a = "Error";
-	try {
-		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
-		const database = client.db('studentdata');
-		const studentdata = database.collection('studentdata');
-		const query = { username: user}
-		a = await studentdata.findOne(query);
-		//console.log(a, "hello")
-		// Send a ping to confirm a successful connection
-		console.log("Pinged your deployment. You successfully connected to MongoDB!");
-	} finally {
-		await client.close();
-	}
-    return a;
-	
-}
-let studentDataset;
-console.log("after Run")
-async function hello() {
-    studentDataset = await run("vik99290").catch(console.dir);
-    console.log(studentDataset);
-}
-hello();
+    let result = "Error";
+    try {
+        await client.connect();
+        const database = client.db('studentdata');
+        const studentdata = database.collection('studentdata');
+        const query = { username: user };
+        result = await studentdata.findOne(query);
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    } finally {
+        await client.close();
+    }
+    return result;
+};
 
+app.get('/:username', async (req, res) => {
+  const { username } = req.params;
+  console.log("Received request for username:", username);
+  try {
+      const student = await run(username);
+      console.log("Retrieved student data:", student);
+      res.json(student);
+  } catch (error) {
+      console.error("Error retrieving student data:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-
-const PORT = 6969
+const PORT = 6969;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
 });
