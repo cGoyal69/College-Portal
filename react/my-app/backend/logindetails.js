6ad30e170a3c5caa5404dbe0e120c7c9d357c520
cGoyal69 <cgoy96@gmail.com>
2024-03-21 14:04:47 +0530

    push

--- react/my-app/backend/logindetails.js
+++ react/my-app/backend/logindetails.js
@@ -2,7 +2,26 @@ const mongoose = require('mongoose');
 const loginDetailsSchema = new mongoose.Schema(
     {
         username: {type: String,unique: true},
-        password: {type: String,unique: true}
+        password: {type: String,unique: true},
+        Allergies: {type:String},
+        "Alternative Contact Number": {type:String},
+        "Blood Group": {type:String},
+        Branch: {type:String},
+        City:{type:String},
+        Contact:{type:String},
+        "Contact Number":{type:String},
+        Domicile:{type:String},
+        "Father Name":{type:String},
+        Hostel:{type:String},
+        "Mother Name":{type:String},
+        Name:{type:String},
+        "Permanent Address":{type:String},
+        "Physical Disablities":{type:String},
+        "Room Numnber":{type:String},
+        Semester:{type:String},
+        State:{type:String},
+        "Year of Graduation":{type:String},
+        "Registration Number":{type:String},
     },
     {
         collection: "LoginInfo",
